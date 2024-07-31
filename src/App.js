a428994a40fc8831a1cfa74b59856b96b6a3fa2a
Henry <henryng161@gmail.com>
2024-07-31 10:49:47 +0700

    Add fossil favicon and change title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import "./App.css";
 import SideMenu from "./components/SideMenu";
 import MainContent from "./components/MainContent";
 
 function App() {
+  useEffect(() => {
+    document.title = "Fossil-Tidio-testbot";
+  }, []);
+
   return (
     <>
       <Header />
